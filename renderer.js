7f3fa1485b9cc46b4f85943dd678906143ad6d27
davawen <56738724+davawen@users.noreply.github.com>
2020-10-11 14:52:35 +0200

    Download

--- renderer.js
+++ renderer.js
@@ -1,44 +1,88 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
 const ytdl = require('ytdl-core');
 const fs = require('fs');
+const homedir = require('os').homedir();
+
 const $ = require('jquery');
 
+/** @type {{name: string, url: string, output: string}[]} List of youtube url */
 const list = [];
 
-function sleep(ms)
-{
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-$('#add').on('click', 
+$('#add').on('click',
 	async () =>
 	{
 		/** @type {JQuery.<HTMLInputElement>} */
-		const searchbox = $('#searchbox');
-		const val = searchbox.val();
-		
-		if(!/((youtube\.com)|(youtu\.be))\//.test(val))
+		const searchBox = $('#searchbox');
+		const outputName = $('#output');
+		const url = searchBox.val();
+
+		if(!/((youtube\.com)|(youtu\.be))\//.test(url))
 		{
-			searchbox.val('');
+			searchBox.val('');
+			outputName.val('');
 			return;
 		}
+
+		const data = await ytdl.getInfo(url);
 		
-		console.log('a');
+		list.push({ name: data.videoDetails.title, url: url, output: outputName.val() });
 		
-		const info = await ytdl.getInfo(val);
+		$('#list').append(`<li><button id="remove">Remove</button>${data.videoDetails.title}</li>`);
 		
-		console.log('b');
-		
-		list.push({name: info.videoDetails.title, id: info.videoDetails.video_url});
-		
-		$('.list').append(`<li>${info.videoDetails.title}</li>`);
-		
-		searchbox.val('');
+		searchBox.val('');
+		outputName.val('');
 	}
 );
 
+$('#list').on('click', 'button',
+	function () //Function required to use this
+	{
+		const name = $(this).parent().contents().filter(function () { return this.nodeType == 3; }).text();
+		
+		$(this).parent().remove();
+		
+		list.splice(list.findIndex(v => v.name === name), 1);
+	}
+);
+
+$('#download').on('click',
+	() =>
+	{
+		
+		
+		list.splice(0).forEach(
+			v =>
+			{
+				let size = 0;
+				let progress = 0;
+				
+				
+				
+				ytdl(v.url, {filter: 'audioonly'})
+					.on('response',
+						function(res)
+						{
+							size = parseInt(res.headers['content-length'], 10);
+						}
+					)
+					.on('data',
+						function(data)
+						{
+							progress += data.length;
+							document.getElementById('loadingForeground').style.width = Math.round(progress/size*100) + '%';
+						}
+					)
+					.pipe(fs.createWriteStream(`${homedir}/Music/${v.output}`))
+					.on('finish',
+						() =>
+						{
+							$('#list').children().eq(1);
+							list.splice(0, 1);
+						}
+					)
+			}
+		);
+		
+		
+		
+	}
+);
